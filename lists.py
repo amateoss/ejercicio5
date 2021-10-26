e881c319cdc49b05a87fbfb0387beb51d569bce2
amateoss <amateoss@alumnos.nebrija.es>
2021-10-26 18:50:55 +0200

    Ejercicio 5

---
+++ lists.py
@@ -0,0 +1,10 @@
+"""find vowels in a word"""
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = []
+for letter in word
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
+print("Vocales: ", found)
+
+
